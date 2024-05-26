2313c535deae8dc89d0f7a03cb1bc2206a42389c
Fredrik Celin <fredrik.celin@captario.com>
2024-05-26 18:47:04 +0930

    Initial streamlit dashboard

--- dashboard/capacity_chart.py
+++ dashboard/capacity_chart.py
@@ -7,34 +7,34 @@ def render_capacity_chart(st_col1, st_col2):
 
     data1 = pd.DataFrame({
         'Timestamp': np.arange(np.datetime64('2023-01-01'), np.datetime64('2024-01-01'), dtype='datetime64[W]'),
-        'Vindkraft (land)': np.random.randint(30, 50, size=52) / 100,
-        'Vindkraft (hav)': np.random.randint(40, 90, size=52) / 100,
-        'Solkraft': np.random.randint(0, 80, size=52) / 100,
-        'Vätgaskraft': np.random.randint(0, 10, size=52) / 100,
-        'Vätgaslagring': np.random.randint(0, 10, size=52) / 100,
-        'Vätgas generering': np.random.randint(0, 10, size=52) / 100,
-        'Batterilagring': np.random.randint(10, 15, size=52) / 100
+        'Vindkraft (land)': np.random.randint(30, 50, size=52) * 1.4,
+        'Vindkraft (hav)': np.random.randint(40, 90, size=52) * 1.4,
+        'Solkraft': np.random.randint(0, 80, size=52) * 1.4,
+        'Vätgaskraft': np.random.randint(0, 10, size=52) * 1.4,
+        'Batteriproduktion': np.random.randint(0, 10, size=52) * 1.4,
+        'Vätgas generering': np.random.randint(0, 20, size=52) * -1,
+        'Batterilagring': np.random.randint(0, 15, size=52) * -1
     })
     data1.set_index('Timestamp', inplace=True)
 
-    col1, col2 = st_col2.columns([1, 1])
+    col1, col2 = st_col2.columns([6, 1])
 
     options = col1.multiselect(
         'Energislag',
         data1.columns.tolist(),
         default=data1.columns.tolist()
     )
-    col2.selectbox("Visa data",
-    ("Kapacitetsfaktor", "MW", "Kostnad"))
+
+    col2.selectbox("", ("MW", "Kostnad"))
 
     if options:
-        plt.figure(figsize=(12, 6))
+        plt.figure(figsize=(20, 8))
 
         for column in options:
-            plt.plot(data1.index, data1[column], label=column)
+            plt.plot(data1.index, data1[column], label=column, linewidth=1)
 
         plt.xlabel('Date')
-        plt.ylabel('Kapacitetsfactor')
+        plt.ylabel('Elproduktion/konsumption')
         plt.title('Produktion')
         plt.legend(loc='upper left')
         plt.grid(True)
