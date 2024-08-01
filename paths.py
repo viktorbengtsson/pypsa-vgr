44237b33a113aca87cfb6838033f26a9d8fa1119
Viktor Bengtsson <viktor@vkbn.ltd>
2024-08-01 20:40:52 +0100

    Remove files before merge to psa-za-wc

--- paths.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-root_path = Path(__file__).resolve().parent
-library_path = root_path / 'library'
-input_path = root_path / 'input'
-output_path = root_path / 'output'
-notebook_path = root_path / 'notebooks'
-dashboard_path = root_path / 'dashboard'
