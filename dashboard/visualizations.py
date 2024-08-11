a882ec3f30d27589eb3b67a92ab5661fa37a4990
Fredrik Celin <fredrik.celin@captario.com>
2024-08-11 23:39:38 +0930

    Restart dashboard

--- dashboard/visualizations.py
+++ dashboard/visualizations.py
@@ -1,112 +1,14 @@
-import seaborn as sns
-from matplotlib.colors import rgb2hex
-import colorsys
+def render_visualizations(st_col1, st_col2, data_root, geography, variables):
 
-def get_labels():
-    return {
-        "Backstop" : "Backstop",
-        "Biogas market" : "Biogas",
-        "Offwind park" : "Vindkraft (hav)",
-        "Onwind park" : "Vindkraft (land)",
-        "Solar park" : "Sol",
-        "H2 storage": "Vätgas",
-        "Battery storage": "Batteri",
-        "Gas turbine": "Gas turbin",
-        "H2 electrolysis": "Vätgas electrolys",
-        "Battery charge": "Batteri laddning",
-        "Biogas input": "Biogas",
-        "SMR nuclear": "Kärnkraft",
-        "Conventional nuclear": "Kärnkraft",
-        "Combined Cycle Gas turbine": "Kombinerad gas turbin",
-        "Simple Cycle Gas turbine": "Enkel gas turbin",
-        "Total": "Total",
-    }
-
-def get_plot_keys(columns):
-    labels = get_labels()
-
-    main_series_labels = []
-    main_series_keys = []
-    for col in columns:
-        if col != "Backstop":
-            main_series_labels.append(labels[col])
-            main_series_keys.append(col)
-
-    return [
-        main_series_labels,
-        main_series_keys,
-    ]
-
-def adjust_lightness(rgb_color, factor):
-    h, l, s = colorsys.rgb_to_hls(*rgb_color)
-    adjusted_l = max(0, min(1, l * factor))
-    return colorsys.hls_to_rgb(h, adjusted_l, s)
-
-def rgb_to_hsl(rgb):
-    factor = 1.0  # Increase lightness by 50%
-    h, l, s = colorsys.rgb_to_hls(*rgb)
-    return h, max(0, min(1, l * factor)), s
-
-def _brighten_colors(colors, use_next_palette):
-    if use_next_palette:
-        return [rgb2hex(adjust_lightness((r, g, b), 1.12)) for r, g, b in colors]
+    if len(geography) == 0:
+        st_col1.write("VÄLJ KOMMUNER TILL VÄNSTER")
     else:
-        return [rgb2hex(color) for color in colors]
+        #config = _config_from_variables(data_root, variables, geography)
 
-def get_plot_colors(main_series_keys, use_next_palette = False):
+        st_col1.write(variables)
+        #st_col1.write(config)
 
-    labels = get_labels()
-    labels_length = len(labels)
+        st_col1.write("Render stuff")
 
-    palette = sns.color_palette("pastel6", labels_length)
-    stor_palette = sns.color_palette('Set3', 4)
-
-    palette = _brighten_colors(palette, use_next_palette)
-    stor_palette = _brighten_colors(stor_palette, use_next_palette)
-
-    colors = {
-        "Backstop" : "black",
-        "Onwind park" : palette[0],
-        "Offwind park" : palette[1],
-        "Solar park" : palette[2],
-        "SMR nuclear": palette[3],
-        "Conventional nuclear": palette[3],
-        "Biogas market" : "red", # Are we showing it?
-        "Biogas input": palette[4],
-        "H2 storage": stor_palette[0],
-        "Battery storage": stor_palette[1],
-        "Combined Cycle Gas turbine": palette[4],
-        "Simple Cycle Gas turbine": palette[4],
-    }
-
-    print(main_series_keys)
-
-    return {k: colors[k] for k in main_series_keys}
-
-def get_plot_label_colors(main_series_keys, use_next_palette = False):
-
-    labels = get_labels()
-    labels_length = len(labels)
-
-    palette = sns.color_palette("pastel6", labels_length)
-    stor_palette = sns.color_palette('Set3', 4)
-
-    palette = _brighten_colors(palette, use_next_palette)
-    stor_palette = _brighten_colors(stor_palette, use_next_palette)
-
-    colors = {
-        "Backstop" : "black",
-        "Onwind park" : palette[0],
-        "Offwind park" : palette[1],
-        "Solar park" : palette[2],
-        "SMR nuclear": palette[3],
-        "Conventional nuclear": palette[3],
-        "Biogas market" : "red", # Are we showing it?
-        "Biogas input": palette[4],
-        "H2 storage": stor_palette[0],
-        "Battery storage": stor_palette[1],
-        "Combined Cycle Gas turbine": palette[4],
-        "Simple Cycle Gas turbine": palette[4],
-    }
-
-    return {labels[k]: colors[k] for k in main_series_keys}
+        st_col2.write("Render widgets")
+    
